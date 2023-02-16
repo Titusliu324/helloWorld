957b30af8008b4ef1c8d0f7ab9d37651b5e54f73
Titus Liu <liut1234@terpmail.umd.edu>
2023-02-15 21:36:44 -0500

    Commit about.html and app.py

--- app.py
+++ app.py
@@ -3,9 +3,13 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/hello')
+@app.route('/')
 def hello_world():  # put application's code here
     return 'Hello World from Titus Liu!'
+
+@app.route('/hello')
+def hello_page():  # put application's code here
+    return 'Hello World from Titus Liu! This is my first HTML page'
 
 @app.route('/about')
 def about():  # put application's code here
