746ccb690365568add3415738f3aabf7b59f7ca0
Titusliu324 <liut1234@terpmail.umd.edu>
2023-03-29 19:40:56 -0400

    added Jinja

--- app.py
+++ app.py
@@ -2,18 +2,24 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def hello_world():
     return 'Hello World from Titus Liu!'
 
 @app.route('/hello')
 def hello():
-    return render_template('hello.html')
+
+    fun_courses = ['BMGT402', 'BMGT382', 'BMGT407']
+
+    return render_template('hello.html', courses=fun_courses)
 
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+@app.route('/favorite-course')
+def about():
+    return render_template('favorite-course.html.html')
 
 if __name__ == '__main__':
     app.run()
